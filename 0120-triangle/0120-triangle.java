fdde52a52fd17d36f43cbc0a6fbabe5017f02c1e
VISHALTHARSHAN-S <vishaltharshansellamuthu@gmail.com>
2026-09-19 22:03:58 +0530

    Time: 5 ms (32.91%), Space: 45.8 MB (33%) - LeetHub

---
+++ 0120-triangle/0120-triangle.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n=triangle.size();
+        int []prev=new int [n];
+        for(int i=0;i<n;i++)
+        prev[i]=triangle.get(n-1).get(i);
+
+        for(int i=n-2;i>=0;i--)
+        {int [] curr=new int[n];
+            for(int j=i;j>=0;j--)
+            {
+                int down=triangle.get(i).get(j)+prev[j];
+                int dia=triangle.get(i).get(j)+prev[j+1];
+                curr[j]=Math.min(down,dia);
+            }
+            prev=curr;
+        }
+
+        return prev[0];
+    }
+}
